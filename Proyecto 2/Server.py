6e7df31b73cda75f7c97b61693e727bb18d58ae7
Ricardo <ricardo.rg462@gmail.com>
2021-10-09 19:11:15 -0300

    ahora si que si rama ricardo

--- Proyecto 2/Server.py
+++ Proyecto 2/Server.py
@@ -5,7 +5,7 @@ import json
 class Server:
     def __init__(self, artefactos_path = "artefactos.json", HOST = '127.0.0.1', PORT = 8889):
         
-
+    # rama ricardo
     # Se cargan los datos de los artefactos.
         with open(artefactos_path, "r") as j:
            self.artefactos = json.load(j)
