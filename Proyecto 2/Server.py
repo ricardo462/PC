ed85255d1cb7fbc4dceeef69e8c0adfec970fc3b
Valeria CZ <valeria.cardoza@ug.uchile.cl>
2021-10-09 22:14:45 -0300

    parte 7 casi lista

--- Proyecto 2/Server.py
+++ Proyecto 2/Server.py
@@ -93,6 +93,9 @@ class Server:
     def cliente(self, sock):
         nombre = self.clientes_dict[sock]
         while True:
+            users = []
+            #Agregué este SEND para hacerlo False en el momento que no quiera mandar un mensaje para todos
+            SEND = True
             try:
                 data = sock.recv(1024).decode()
             except:
@@ -116,16 +119,156 @@ class Server:
             elif data == ":artefactos":
                 # Se crea una lista con los nombres (no números) de los artefactos.
                 arte_list = [self.artefactos.get(artefacto) if self.artefactos.get(artefacto) is not None else '' for artefacto in self.arte_clientes[sock]]
-                sock.send(f"[SERVER] Tus artefactos son {', '.join(arte_list)}".encode())
+                sock.send(f"[SERVER] Tus artefactos son {', '.join(arte_list)}\n".encode())
+                SEND = False
 
             elif data == ":larva":
                 data = "(:o)OOOooo"
 
-            # Se manda el mensaje a todos los clientes.
-            for s in self.sock_clientes:
-                if s != sock:
-                    s.send(f"CLIENTE {nombre}: {data}".encode())
-                else:
-                    s.send(f"Yo: {data}".encode())
+            elif data == ":u":
+                #Creamos lista con nombres de los usuarios conectados
+                for client in self.sock_clientes:
+                    name = self.clientes_dict[client]
+                    users.append(name)
+
+            elif data == ":smile":
+                data = ":)"
+
+            elif data == ":angry":
+                data = ">:("
+            
+            elif data == ":combito":
+                data = "Q('-'Q)"
+            
+            elif data[:10] == ":artefacto" and data != ":artefactos":
+                arteID = data[11:]
+                SEND = False
+                if int(arteID) < 1 or int(arteID)>42:
+                    sock.send(f"[SERVER] Ese artefacto no existe:(\n".encode())
+                else: 
+                    artefacto = self.artefactos[arteID]
+                    sock.send(f"[SERVER] El artefacto {arteID} corresponde a {artefacto}\n".encode())
+
+            elif data[:2] == ":p":
+                SEND = False
+                #Agregamos usuarios conectados a una lista
+                new_users = []
+                for client in self.sock_clientes:
+                    name = self.clientes_dict[client]
+                    new_users.append(name)
+                
+                #Vemos a qué usuario queremos mandarle el susurro
+                for user in new_users:
+                    largo = len(user)
+                    if data[3:3+largo] == user:
+                        Id = data[3:3+largo]
+                
+                
+                #Valor de conexión del cliente al cual le enviaremos el mensaje
+                for client in self.clientes_dict:
+                    if Id == self.clientes_dict[client]:
+                        whisper_to = client
+                
+                #whisper_to: socket, sock: socket (qn envia)
+                mensaje = data[len(Id)+3:]
+                sock.send(f"*susurro* Yo: {mensaje}".encode())
+                whisper_to.send(f"*susurro* CLIENTE {nombre}: {mensaje}".encode())
+
+            elif data[:6] == ":offer":
+                SEND = False
+                new_users = []
+                for client in self.sock_clientes:
+                    name = self.clientes_dict[client]
+                    new_users.append(name)
+                
+                #Vemos a con quien queremos hacer el intercambio
+                for user in new_users:
+                    largo = len(user)
+                    if data[3:3+largo] == user:
+                        Id = data[3:3+largo]
+
+
+                for client in self.clientes_dict:
+                    if Id == self.clientes_dict[client]:
+                        exc_with = client
+
+                largo = len(Id)
+
+                artId = data[6+largo:6+largo+2]
+
+                if int(artId) < 10:
+                    suArtId = data[6+largo+1:]
+
+                if int(artId) >= 10:
+                    suArtId = data[6+largo+2:]
+
+                intercambio_thread = threading.Thread(target=self.intercambio, args=(sock, exc_with, artId, suArtId))
+                intercambio_thread.start()
+
+
+            # Se manda el mensaje a todos los clientes si SEND es True.
+            if SEND:
+                for s in self.sock_clientes:
+                    if s != sock:
+                        if len(users)>0:
+                            s.send(f"[SERVER] Los usuarios conectados son {', '.join(users)}".encode())
+                        else: 
+                            s.send(f"CLIENTE {nombre}: {data}".encode())
+                    else:
+                        if len(users)>0:
+                            s.send(f"[SERVER] Los usuarios conectados son {', '.join(users)}".encode())
+                        else: 
+                            s.send(f"Yo: {data}".encode())
 
     
+    def intercambio(self, client1, client2, artId1, artId2):
+        while True:
+            artName1 = self.artefactos[artId1]
+            artName2 = self.artefactos[artId2]
+
+            name1 = self.clientes_dict[client1]
+            name2 = self.clientes_dict[client2]
+
+            client1.send(f"Yo: Quiero intercambiar un {artName1} por un {artName2} con {name2}".encode())
+            client2.send(f"[SERVER] CLIENTE {name1} quiere intercambiar un {artName1} por un {artName2}\n".encode())
+            client2.send(f"Deseas proceder con el intercambio?[:accept/:reject]".encode())
+
+            try:
+                data = client2.recv(1024).decode()
+            except:
+                break
+
+            if data==":accept":
+                #procedemos con el intercambio
+                if artId1 not in self.arte_clientes[client1]:
+                    client1.send(f"[SERVER] No posees {artName1}. Se cancela el intercambio.".encode())
+                    client2.send(f"[SERVER] {name1} no posee {artName1}. Se cancela el intercambio.".encode())
+                    break
+
+                elif artId2 not in self.arte_clientes[client2]:
+                    client2.send(f"[SERVER] No posees {artName2}. Se cancela el intercambio.".encode())
+                    client1.send(f"[SERVER] {name2} no posee {artName2}. Se cancela el intercambio.".encode())
+                    break
+
+                elif artId1 in self.arte_clientes[client1] and artId2 in self.arte_clientes:
+                    art_cliente1 = self.arte_clientes[client1]
+                    art_cliente2 = self.arte_clientes[client2]
+
+                    art_cliente1.remove(artId1)
+                    art_cliente2.remove(artId2)
+
+                    art_cliente1.append(artId2)
+                    art_cliente2.append(artId1)
+
+                    client1.send(f"[SERVER] Intercambio exitoso.".encode())
+                    client2.send(f"[SERVER] Intercambio exitoso.".encode())
+
+                    break
+
+
+            elif data==":reject":
+                client1.send(f"[SERVER] {name2} rechazó el intercambio.")
+                client2.send(f"[SERVER] Cancelaste el intercambio.")
+
+            elif data!=":accept" or data!=":reject":
+                client2.send(f"[SERVER] Perdón, solo entiendo :accept o :reject :(".encode())
